44b93f71a3e60cb0658168cd076487b4b0616f21
thewhostommy <a.kasper@srb.at>
2020-08-05 13:46:51 +0200

    Added Source

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # HashCheck
  Compare directories using hashes against an intermediate file
+
+## Usage
+### Create hash file
+```ps
+hashcheck.ps1 -generatehash -hashdir <directory to hash> -hashfile <file to store hashes>
+```
+
+### Compare directory against intermediate hash file
+```ps
+hashcheck.ps1 -hashfile <file read hashes>
+```
---
+++ hashcheck.ps1
@@ -0,0 +1,55 @@
+﻿param(
+[switch]$generatehash,
+[string]$hashfile,
+[string]$hashdir
+)
+
+$Hashstorefile = $hashfile
+
+if ( $generatehash -eq $true )
+{
+    Write-Host "Generating hash-file $Hashstorefile for directory $hashdir"
+
+    $Hashstore = dir $hashdir -Recurs | Get-FileHash | ConvertTo-Csv
+
+    $Hashstore | Out-file $Hashstorefile
+
+    exit
+}
+################################
+
+
+[int]$countdifffiles = 0
+[int]$countmissingfiles = 0
+
+Write-Host "Checking files agianst hash-file $Hashstorefile"
+Write-Host "---------------------------------------------"
+
+$hashtab = Get-Content $Hashstorefile | ConvertFrom-Csv 
+
+ForEach ($oldhash in $hashtab)
+{
+    $filepath = $oldhash.Path
+
+    if ( Test-Path $oldhash.Path )
+    {
+        $newhash = Get-FileHash $oldhash.Path
+
+        if ( $newhash.Hash -ne $oldhash.hash  )
+        {
+            write-host "Different file: $filepath" -ForegroundColor "Red"
+			
+			$countdifffiles = $countdifffiles + 1
+        }
+    }
+    else
+    {
+        write-host "Missing file: $filepath" -ForegroundColor "Red"
+		
+		$countmissingfiles = $countmissingfiles + 1
+    }
+}
+
+Write-Host "---------------------------------------------"
+Write-Host "Different files count: $countdifffiles"
+Write-Host "Missing files count: $countmissingfiles"
